2f9c08dc1f9e00bc6a2588638132fb68f56d7fa1
achnitreda <achnitreda@gmail.com>
2022-11-30 18:05:19 +0100

    change mongodb_uri

--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,10 @@ process.on("uncaughtException", (err) => {
 const app = require("./app");
 
 // Connect mongoose with mongodb Atlas
-// const db = `mongodb+srv://reda42:${process.env.DATABASE_PASS}@cluster0.dqn8fht.mongodb.net/jwt?retryWrites=true&w=majority`;
+const db = `mongodb+srv://reda42:${process.env.DATABASE_PASS}@cluster0.dqn8fht.mongodb.net/jwt?retryWrites=true&w=majority`;
 
 // https://www.mongodb.com/community/forums/t/suddenly-unable-to-connect-querytxt-etimeout/117864 OLD VERSION 
-const db = `mongodb://reda42:${process.env.DATABASE_PASS}@ac-uunlnw5-shard-00-00.dqn8fht.mongodb.net:27017,ac-uunlnw5-shard-00-01.dqn8fht.mongodb.net:27017,ac-uunlnw5-shard-00-02.dqn8fht.mongodb.net:27017/?ssl=true&replicaSet=atlas-muvecw-shard-0&authSource=admin&retryWrites=true&w=majority`;
+// const db = `mongodb://reda42:${process.env.DATABASE_PASS}@ac-uunlnw5-shard-00-00.dqn8fht.mongodb.net:27017,ac-uunlnw5-shard-00-01.dqn8fht.mongodb.net:27017,ac-uunlnw5-shard-00-02.dqn8fht.mongodb.net:27017/?ssl=true&replicaSet=atlas-muvecw-shard-0&authSource=admin&retryWrites=true&w=majority`;
 mongoose
   .connect(db, {
     useNewUrlParser: true,
